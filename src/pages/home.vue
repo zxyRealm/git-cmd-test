7b6275b79e5ae13d3601a2ab914d31514cd9b57b
刃下心 <1936689502@qq.com>
2019-01-24 00:32:07 +0800

    dev 初次提交

---
+++ src/pages/home.vue
@@ -0,0 +1,21 @@
+<template>
+ <div class="page">
+
+ </div>
+</template>
+
+<script>
+export default {
+ data() {
+ return {
+
+ }
+ },
+ components: {
+
+ }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+ <div class="page">
+    <h2>分支提交了</h2>
+ </div>
+</template>
+
+<script>
+export default {   
+ data() {
+    return {
+    }
+ },
+ components: {
+
+ }
+}
+</script>
+
+<style scoped>
+</style>

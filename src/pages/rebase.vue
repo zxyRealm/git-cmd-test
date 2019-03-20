e93d700a335674e9f36148ed7e459003b31ec32e
刃下心 <1936689502@qq.com>
2019-03-21 00:50:35 +0800

    feat(Rebase): add a test list

--- src/pages/rebase.vue
+++ src/pages/rebase.vue
@@ -1,5 +1,11 @@
 <template>
-    
+    <div class="rebase-wrap">
+        <ul class="list-wrap">
+            <li class="list-item" v-for="i in 8" :key="i">
+                这是第{{i}}个
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
